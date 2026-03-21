a674e05d7cd8f8b02a42cc863712a8902f8a9a74
jefersontave@gmail.com <jefersontave@gmail.com>
2026-03-21 19:08:51 -0300

    corrige senha

--- js/script.js
+++ js/script.js
@@ -519,7 +519,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('btn-unlock-final').addEventListener('click', () => {
         const resposta = document.getElementById('final-answer').value.toLowerCase().trim();
         
-        if (resposta === "Eu te amo") {
+        if (resposta === "eu te amo") {
             mostrarFinalFeliz();
         } else {
             playLose(); 
